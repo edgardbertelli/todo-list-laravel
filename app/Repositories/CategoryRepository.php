9d1fe4fdd76604817ffba3bd3392812fb9c8515b
Edgard Bertelli <bertelliedgard@gmail.com>
2023-11-15 19:42:34 -0300

    Feat: cria a tabela de sessão

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repositories;
 
 use App\Contracts\CategoryContract;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -28,14 +27,14 @@ class CategoryRepository implements CategoryContract
     /**
      * Creates a new category.
      * 
-     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $validated
      * @return void
      */
-    public function store(Request $request)
+    public function store(array $validated)
     {
         $category = DB::table('categories')->insert([
-            'name' => $request->input('name'),
-            'slug' => Str::slug($request->input('name')),
+            'name' => $validated['name'],
+            'slug' => Str::slug($validated['name']),
             'user_id' => Auth::user()->id,
             'created_at' => now(),
         ]);
@@ -61,22 +60,22 @@ class CategoryRepository implements CategoryContract
     /**
      * Updates a category.
      * 
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\UpdateCategoryRequest  $request
      * @param  string  $slug
      */
-    public function update(Request $request, string $slug)
+    public function update(array $validated, string $slug)
     {
         DB::table('categories')
            ->where('slug', $slug)
            ->where('user_id', Auth::user()->id)
            ->update([
-                'name' => $request->input('name'),
-                'slug' => Str::slug($request->input('name')),
+                'name' => $validated['name'],
+                'slug' => Str::slug($validated['name']),
                 'updated_at' => now()
            ]);
 
         $category = DB::table('categories')
-                       ->where('slug', Str::slug($request->input('name')))
+                       ->where('slug', Str::slug($validated['name']))
                        ->where('user_id', Auth::user()->id)
                        ->first();
 
