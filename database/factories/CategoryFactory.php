39898c2f3fa4977a0d0a1c5a517cc3a4c409229d
Edgard Bertelli <bertelliedgard@gmail.com>
2023-11-25 18:33:36 -0300

    Feat: cria factories de usuários e categorias

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,9 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->unique()->word(),
+            'slug' => fake()->unique()->slug(),
+            'user_id' => 1
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Edgard',
+            'email' => 'bertelliedgard@gmail.com',
+            'username' => 'edgardbertelli'
+        ]);
+
+        \App\Models\User::factory(100)->create();
+
+        \App\Models\Category::factory()->count(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
